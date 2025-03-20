b1d22b83e4c3a78440561e3d4b7df2e454414a34
Pratik07007 <s.dhimal006@gmail.com>
2025-03-20 07:52:33 +0545

    feat:addition

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hwllo World")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Please Enter 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division\n")
+
+choice = int(input("Enter your choice (1-4): "))
+
+if choice == 1:
+    result = num1 + num2
+    print(f"Result: {num1} + {num2} = {result}")
+
